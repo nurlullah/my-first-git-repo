249d3ae5b097354da0f7b3897024bd30a7f0fb87
Nurullah Demirkoparan <nurlullahhh@gmail.com>
2022-07-16 22:47:17 +0300

    third commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -1,6 +1,8 @@
 public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("I am learning GitHub!!");
+        System.out.println("I am adding new code...");
+        System.out.println("I am adding one more");
     }
 
 
